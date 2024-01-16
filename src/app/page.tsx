67f389fa0e6e1e0c211da064cf6d572bece3167e
Fajar Maftuh Fadli <fajarmaftuhfadli@gmail.com>
2024-01-16 14:23:57 +0700

    refactor(page.tsx): restructure the layout of the Home component to improve readability and maintainability
    The changes in this commit restructure the layout of the Home component in the page.tsx file. The previous layout had two sections, one for the Hero component and one for the About component. The new layout divides the main content into two columns using flexbox. The left column contains the Hero component, and the right column contains the About component and the newly added Experience component.
    
    This restructuring improves the organization of the code and makes it easier to understand the structure of the page. It also allows for better responsiveness on larger screens by using the lg:flex-row and lg:w-1/2 classes to control the layout on larger screens.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,22 @@
 export default function Home() {
   return (
-    <main className="flex flex-col gap-20 px-6 py-14">
-      <section>
-        <Hero />
-      </section>
-      <section>
-        <About />
-      </section>
+    <main className="">
+      <div className="mx-auto flex max-w-5xl flex-col gap-20 px-6 py-14 lg:flex-row lg:gap-10">
+        <div className="lg:w-1/2">
+          <Hero />
+        </div>
+        <div className="flex flex-col gap-20 lg:w-1/2">
+          <About />
+          <Experience />
+        </div>
+      </div>
     </main>
   );
 }
 
 function Hero() {
   return (
-    <div className="flex flex-col gap-10 text-slate-800">
+    <section className="flex flex-col gap-10 text-slate-800">
       <div>
         <p className="text-2xl font-semibold">Hello, I'm</p>
         <h1 className="text-5xl font-bold">Fajar Fadli</h1>
@@ -37,13 +40,13 @@ function Hero() {
           </a>
         </li>
       </ul>
-    </div>
+    </section>
   );
 }
 
 function About() {
   return (
-    <div>
+    <section>
       <h2 className="text-3xl font-semibold">About</h2>
       <p>
         Labore reiciendis, perferendis tempore earum explicabo porro voluptas
@@ -61,6 +64,21 @@ function About() {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
         laboriosam nostrum corrupti beatae.
       </p>
-    </div>
+    </section>
+  );
+}
+
+function Experience() {
+  return (
+    <section>
+      <h2 className="text-3xl font-semibold">Experience</h2>
+      <ul>
+        <li>
+          <h3>Job 1</h3>
+          <p>Sep 2022 - Present</p>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+      </ul>
+    </section>
   );
 }
