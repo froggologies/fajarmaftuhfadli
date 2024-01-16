d2af6874211a7042b6e2f86f6912a7040d146c68
Fajar Maftuh Fadli <fajarmaftuhfadli@gmail.com>
2024-01-16 18:06:31 +0700

    refactor(page.tsx): simplify the structure of the Home component by removing unnecessary divs and reorganizing the code for better readability feat(page.tsx): add a background color and text color to the main container to improve the visual appearance refactor(page.tsx): simplify the structure of the Hero component by removing unnecessary divs and reorganizing the code for better readability feat(page.tsx): add a background image and additional information to the Hero component to enhance the user experience refactor(page.tsx): remove the About and Experience components as they are not currently being used feat(page.tsx): add a DisplayPicture component to display a profile picture in the Hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,84 +1,47 @@
 export default function Home() {
   return (
-    <main className="">
-      <div className="mx-auto flex max-w-5xl flex-col gap-20 px-6 py-14 lg:flex-row lg:gap-10">
-        <div className="lg:w-1/2">
-          <Hero />
-        </div>
-        <div className="flex flex-col gap-20 lg:w-1/2">
-          <About />
-          <Experience />
-        </div>
-      </div>
+    <main className="bg-base text-white">
+      <Hero />
     </main>
   );
 }
 
 function Hero() {
   return (
-    <section className="flex flex-col gap-10 text-slate-800">
+    <>
       <div>
-        <p className="text-2xl font-semibold">Hello, I'm</p>
-        <h1 className="text-5xl font-bold">Fajar Fadli</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <div className="bg-crust h-32 w-full"></div>
+        <div className="relative px-4">
+          <DisplayPicture />
+          <div className="flex flex-col gap-4">
+            <div>
+              <p className="text-xl font-bold">Fajar Maftuh Fadli</p>
+              <p>@fajarmaftuhfadli</p>
+            </div>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla,
+              mollitia.
+            </p>
+            <div>
+              <ul>
+                <li>Depok, Indonesia</li>
+                <li>linkedin link</li>
+                <li>Last updated: January 2023</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
-      <ul className="font-semibold">
-        <li>
-          <a href="" className="group flex items-center gap-2 text-[#FF4F00]">
-            <div className="h-1 w-10 bg-slate-800 group-hover:w-10"></div>About
-          </a>
-        </li>
-        <li>
-          <a href="" className="group flex items-center gap-2">
-            <div className="h-1 w-5 bg-slate-800 group-hover:w-10"></div>
-            Experience
-          </a>
-        </li>
-        <li>
-          <a href="" className="group flex items-center gap-2">
-            <div className="h-1 w-5 bg-slate-800 group-hover:w-10"></div>Project
-          </a>
-        </li>
-      </ul>
-    </section>
+    </>
   );
 }
 
-function About() {
+function DisplayPicture() {
+  // The parent need to have display relative
   return (
-    <section>
-      <h2 className="text-3xl font-semibold">About</h2>
-      <p>
-        Labore reiciendis, perferendis tempore earum explicabo porro voluptas
-        eveniet accusantium?
-      </p>
-      <br />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic laborum
-        natus iure consectetur dolorum odio explicabo tenetur temporibus
-        exercitationem optio! Nostrum aut numquam sint reiciendis laborum saepe
-        maiores culpa pariatur?
-      </p>
-      <br />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-        laboriosam nostrum corrupti beatae.
-      </p>
-    </section>
-  );
-}
-
-function Experience() {
-  return (
-    <section>
-      <h2 className="text-3xl font-semibold">Experience</h2>
-      <ul>
-        <li>
-          <h3>Job 1</h3>
-          <p>Sep 2022 - Present</p>
-          <p>Lorem ipsum dolor sit amet.</p>
-        </li>
-      </ul>
-    </section>
+    <>
+      <div className="bg-mantle border-base absolute top-0 h-24 w-24 -translate-y-1/2 rounded-full border-2"></div>
+      <div className="mb-4 h-12 w-24"></div> {/** ghost component */}
+    </>
   );
 }
