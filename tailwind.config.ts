67242af2048bb25bb75a8669329fb6c6f4b9701e
Fajar Maftuh Fadli <fajarmaftuhfadli@gmail.com>
2024-01-16 17:51:09 +0700

    chore(package.json): update framer-motion dependency to @react-spring/web version 9.7.3 chore(tailwind.config.ts): update colors in the theme configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,21 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        text: "#cad3f5",
+        base: "#24273a",
+        mantle: "#1e2030",
+        crust: "#181926",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
