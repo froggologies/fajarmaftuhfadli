6ff8c6dabd20d68a0d6ec2a1418917ac732e6c37
Fajar Maftuh Fadli <fajarmaftuhfadli@gmail.com>
2024-01-22 10:57:47 +0700

    chore(Header.tsx): update CSS class in Header component to fix styling issue

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <div className="sticky top-0 z-20 bg-mantle/80 px-4 py-2 backdrop-blur-sm lg:hidden">
+    <div className="#bg-mantle/80 fixed top-0 z-20 w-full px-4 py-2 backdrop-blur-sm lg:hidden">
       <div className="mx-auto flex w-full max-w-lg items-center justify-between sm:max-w-xl md:max-w-2xl">
         <div className="flex flex-col">
           <span className="text-lg font-semibold">Frog</span>
